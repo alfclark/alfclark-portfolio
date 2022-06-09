e2ad4133ec7ff15416ccb792ea3b3c0f940cbdd1
AlfClark <alfredoclarkcardenas@gmail.com>
2022-06-08 21:06:23 -0700

    Under Construction element base

---
+++ src/components/UnderConstruction.vue
@@ -0,0 +1,9 @@
+<template>
+  <div
+    class="font-head text-center py-3 px-1 bg-red-lighter text-red-darker text-sm font-light"
+  >
+    <p>🏗 Site is currently under construction 🏗</p>
+  </div>
+</template>
+
+<style scoped></style>
